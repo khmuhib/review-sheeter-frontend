933992e5f1db285836c1680833a09e6431d5a59b
khmuhib <khmuhib2013@gmail.com>
2022-08-13 13:44:46 +0600

    first commit

---
+++ components/inc/AppheaderTop.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="py-3 bg-warning">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <NuxtLink to="/"
+            ><img src="/images/logo.png" alt="" class="header-top-logo-img"
+          /></NuxtLink>
+        </div>
+        <div class="col-md-6 text-right">
+          <a href="#">
+            <img
+              src="/images/header_top_ads.jpg"
+              alt=""
+              class="header-top-ads-img"
+            />
+          </a>
+        </div>
+        <div class="col-md-3 text-right d-flex align-items-center">
+          <nuxt-link :to="{ name: 'login' }" class="btn btn-success m-2" v-if="!loggedIn">Login</nuxt-link>
+          <nuxt-link :to="{ name: 'register' }" class="btn btn-primary m-2" v-if="!loggedIn">Sign up</nuxt-link>
+          <nuxt-link :to="{ name: 'dashboard' }" class="btn btn-primary m-2" v-if="loggedIn">Dashboard</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.loggedIn;
+    },
+
+  }
+};
+</script>
+
+<style scoped>
+</style>
